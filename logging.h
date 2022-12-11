4bc4bf36ea12324302cf8071fab859dfb10cf617
Edd Inglis <edd.inglis@gmail.com>
2022-12-11 16:24:47 +0000

    basic looging helpers

---
+++ logging.h
@@ -0,0 +1,46 @@
+#pragma ONCE
+
+class Logger
+{
+public:
+  Logger( const __FlashStringHelper* source )
+    : src{ source }
+    { }
+  
+  #define DEFINE_LOG_FNS( level, tag ) \
+    void level( const __FlashStringHelper* msg ) { log( F(": " tag ": "), msg ); } \
+    void level( const char* msg ) { log( F(": " tag ": "), msg ); } \
+    void level##f( const char* fmt, ... ) \
+    { \
+      va_list args; \
+      va_start(args, fmt); \
+      vsnprintf( temp, sizeof(temp), fmt, args); \
+      va_end(args); \
+      level( temp ); \
+    }
+
+  DEFINE_LOG_FNS( debug,   "DEBUG" )
+  DEFINE_LOG_FNS( info,    "INFO" )
+  DEFINE_LOG_FNS( warning, "WARNING" )
+  DEFINE_LOG_FNS( error,   "ERROR" )
+
+  #undef DEFINE_LOG_FNS
+
+private:
+  const __FlashStringHelper* src;
+  char temp[ 128 ]; // deliberately short-ish; could probably be static...
+
+  void log( const __FlashStringHelper* level_etc, const char* msg )
+  {
+    // to save operations, print operations, level_etc should be of the form  ": level: " 
+    Serial.print( src );
+    Serial.print( level_etc );
+    Serial.println( msg );
+  }
+  void log( const __FlashStringHelper* level_etc, const __FlashStringHelper* msg )
+  {
+    Serial.print( src );
+    Serial.print( level_etc );
+    Serial.println( msg );
+  }
+};
