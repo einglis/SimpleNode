6ad0ad74944d48babe07ce3065c4432f1a075933
Edd Inglis <edd.inglis@gmail.com>
2023-01-15 17:58:04 +0000

    same again for ntp

---
+++ node/ntp.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include <NTPClient.h>
+#include <Ticker.h>
+#include <WiFiUdp.h>
+
+#include "../logging.h"
+#include "wifi_observer.h"
+
+namespace node {
+
+class Ntp
+  : public node::WifiObserver
+{
+public:
+  Ntp()
+    : client( my_wifi, NTP_HOST/*lazy*/ )
+  { }
+
+  void setup( int report_interval = 0 )
+  {
+    if (report_interval)
+      report_ticker.attach_scheduled( report_interval, [this](){
+        log.infof( "time: %s", client.getFormattedTime() );
+      } );
+
+    node::WifiObservers::add( this );
+  }
+
+  long int epoch_time( ) { return client.getEpochTime(); }
+
+  static int epoch_day( long int e )  { return (e / 86400L + 4) % 7; } // 0 == sunday
+  static int epoch_hrs( long int e )  { return (e / 3600) % 24; }
+  static int epoch_mins( long int e ) { return (e / 60) % 60; }
+  static int epoch_secs( long int e ) { return  e % 60; }
+
+  virtual void wifi_up() // WifiObserver
+  {
+    client.begin();
+    refresh();
+  }
+
+  virtual void wifi_down() // WifiObserver
+  {
+    refresh_ticker.detach();
+    client.end();
+  }
+
+private:
+  WiFiUDP my_wifi;
+  NTPClient client;
+  Ticker report_ticker;
+  Ticker refresh_ticker;
+
+  void refresh( int phase = 0 )
+  {
+    log.debug( F("refresh") );
+
+    int wait;
+    if (client.update())
+    {
+      wait = 10 * 60; // ten minutes after success
+      phase = 0; // restart at 1 second on next fail
+    }
+    else
+    {
+      wait = 1 << phase; // 1, 2, 4,... seconds
+      phase = min( phase + 1, 5 ); // 1 << 5 == 32 seconds
+    }
+
+    refresh_ticker.once_scheduled( wait, [this, phase](){ refresh( phase ); } );
+  }
+
+  static Logger log;
+};
+
+// ------------------------------------
+
+Logger Ntp::log( "NTP" );
+
+} //node
--- simple_node.ino
+++ simple_node.ino
@@ -1,9 +1,6 @@
 
 #include <Adafruit_NeoPixel.h>
-#include <ESPAsyncWebServer.h>
-#include <NTPClient.h>
 #include <Ticker.h>
-#include <WiFiUdp.h>
 
 #include "app_config.h"
 #include "build.gen.h"
@@ -14,6 +11,9 @@ const char *Version = XXX_BUILD_REPO_VERSION " (" XXX_BUILD_DATE ")";
 
 #include "node/configuration.h"
 node::Configuration configuration;
+
+#include "node/ntp.h"
+node::Ntp ntp;
 
 #include "node/patterns.h"
 node::Patterns patterns;
@@ -31,80 +31,6 @@ node::WiFi wifi;
 
 #include "node/mqtt.h" // requires wifi
 node::Mqtt mqtt;
-
-// ----------------------------------------------------------------------------
-
-#ifdef NODE_HAS_NTP
-
-class Ntp
-  : public node::WifiObserver
-{
-public:
-  Ntp()
-    : client( my_wifi, NTP_HOST/*lazy*/ )
-  { }
-
-  void setup( int report_interval = 0 )
-  {
-    if (report_interval)
-      report_ticker.attach_scheduled( report_interval, [this](){
-        log.infof( "time: %s", client.getFormattedTime() );
-      } );
-
-    node::WifiObservers::add( this );
-  }
-
-  long int epoch_time( ) { return client.getEpochTime(); }
-
-  static int epoch_day( long int e )  { return (e / 86400L + 4) % 7; } // 0 == sunday
-  static int epoch_hrs( long int e )  { return (e / 3600) % 24; }
-  static int epoch_mins( long int e ) { return (e / 60) % 60; }
-  static int epoch_secs( long int e ) { return  e % 60; }
-
-  virtual void wifi_up() // WifiObserver
-  {
-    client.begin();
-    refresh();
-  }
-
-  virtual void wifi_down() // WifiObserver
-  {
-    refresh_ticker.detach();
-    client.end();
-  }
-
-private:
-  WiFiUDP my_wifi;
-  NTPClient client;
-  Ticker report_ticker;
-  Ticker refresh_ticker;
-
-  void refresh( int phase = 0 )
-  {
-    log.debug( F("refresh") );
-
-    int wait;
-    if (client.update())
-    {
-      wait = 10 * 60; // ten minutes after success
-      phase = 0; // restart at 1 second on next fail
-    }
-    else
-    {
-      wait = 1 << phase; // 1, 2, 4,... seconds
-      phase = min( phase + 1, 5 ); // 1 << 5 == 32 seconds
-    }
-
-    refresh_ticker.once_scheduled( wait, [this, phase](){ refresh( phase ); } );
-  }
-
-  static Logger log;
-};
-
-Ntp ntp;
-Logger Ntp::log( "NTP" );
-
-#endif
 
 // ----------------------------------------------------------------------------
 
