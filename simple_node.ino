7296c1da758f62fa843541370a1434c77ccd05b5
Edd Inglis <edd.inglis@gmail.com>
2023-01-19 21:54:05 +0000

    consolidating similar blocks, now dependencies have been removed

--- simple_node.ino
+++ simple_node.ino
@@ -3,36 +3,29 @@
 #include "build.gen.h"
 const char *Version = XXX_BUILD_REPO_VERSION " (" XXX_BUILD_DATE ")";
 
+#include "node/configuration.h"
 #include "node/inputs.h"
 #include "node/logging.h"
+#include "node/mqtt.h"
+#include "node/ntp.h"
+#include "node/patterns.h"
+#include "node/pixels.h"
+#include "node/uptime.h"
+#include "node/webserver.h"
+#include "node/wifi.h"
 
 // ----------------------------------------------------------------------------
 
-#include "node/configuration.h"
 node::Configuration< AppConfig > configuration( CONFIG_FILENAME );
-
-#include "node/mqtt.h"
 node::Mqtt mqtt;
-
-#include "node/ntp.h"
 node::Ntp ntp;
-
-#include "node/patterns.h"
 node::WifiPatterns patterns( node::outputs::status_pin );
-
-#include "node/pixels.h"
 node::Pixels pixels( NUM_PIXELS, node::outputs::pixels_pin );
-
-#include "node/uptime.h"
 node::Uptime uptime;
-
-#include "node/webserver.h"
 node::Webserver web;
-
-#include "node/wifi.h"
 node::WiFi wifi;
 
-// ----------------------------------------------------------------------------
+// ------------------------------------
 
 void setup( )
 {
@@ -43,28 +36,17 @@ void setup( )
   Serial.println( Version );
   Serial.println( ESP.getResetReason() );
 
+  configuration.begin();
+
   uptime.begin();
   patterns.begin();
-
-#ifdef NODE_HAS_CONFIG
-  configuration.begin();
-#endif
+  pixels.begin();
 
   wifi.begin();
-
-#ifdef NODE_HAS_NTP
   ntp.begin( 11 /*report interval in seconds*/ );
-#endif
-#ifdef NODE_HAS_MQTT
   mqtt.begin( );
-#endif
-#ifdef NODE_HAS_PIXELS
-  pixels.begin();
-#endif
-#ifdef NODE_HAS_WEB
   web.begin();
-  register_web_pages( web );
-#endif
+  register_web_pages( web ); // move to app_setup?
 
   app_setup();
 }
