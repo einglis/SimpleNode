0db869f440fda7d24b7344873c2a2e062536ec30
Edd Inglis <edd.inglis@gmail.com>
2023-01-19 22:00:08 +0000

    documentation tweak

--- simple_node.ino
+++ simple_node.ino
@@ -6,10 +6,10 @@ const char *Version = XXX_BUILD_REPO_VERSION " (" XXX_BUILD_DATE ")";
 #include "node/configuration.h"
 #include "node/inputs.h"
 #include "node/logging.h"
-#include "node/mqtt.h"
+#include "node/mqtt.h" // costs about 9k loops/sec
 #include "node/ntp.h"
 #include "node/patterns.h"
-#include "node/pixels.h"
+#include "node/pixels.h" // costs about 3k loops/sec
 #include "node/uptime.h"
 #include "node/webserver.h"
 #include "node/wifi.h"
