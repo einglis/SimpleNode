dd2e83c5a321b76bfff95b6b77c38cd40e431cde
Edd Inglis <edd.inglis@gmail.com>
2023-01-21 14:05:07 +0000

    gathering all node headers into a single library-friendly one

---
+++ simple_node.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "node/defaults.h" // needs to come first for now
+
+#include "node/configuration.h"
+#include "node/inputs.h"
+#include "node/logging.h"
+#include "node/mqtt.h" // costs about 9k loops/sec
+#include "node/ntp.h"
+#include "node/patterns.h"
+#include "node/pixels.h" // costs about 3k loops/sec
+#include "node/uptime.h"
+#include "node/webserver.h"
+#include "node/wifi.h"
--- simple_node.ino
+++ simple_node.ino
@@ -3,17 +3,7 @@
 #include "build.gen.h"
 const char *Version = XXX_BUILD_REPO_VERSION " (" XXX_BUILD_DATE ")";
 
-#include "node/defaults.h" // needs to come first for now
-#include "node/configuration.h"
-#include "node/inputs.h"
-#include "node/logging.h"
-#include "node/mqtt.h" // costs about 9k loops/sec
-#include "node/ntp.h"
-#include "node/patterns.h"
-#include "node/pixels.h" // costs about 3k loops/sec
-#include "node/uptime.h"
-#include "node/webserver.h"
-#include "node/wifi.h"
+#include "simple_node.h"
 
 // ----------------------------------------------------------------------------
 
