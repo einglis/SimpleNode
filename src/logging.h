ffc0586b768979eb895e3e78136d2bcf888c9dea
Edd Inglis <edd.inglis@gmail.com>
2023-06-25 20:56:15 +0100

    a lazy syslogging addition

--- src/logging.h
+++ src/logging.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#include <WiFiUdp.h>
+
+#include "logging.h"
+
 namespace node {
 
 class Logger
@@ -38,13 +42,44 @@ private:
     Serial.print( src );
     Serial.print( level_etc );
     Serial.println( msg );
+
+    syslog(1, src, msg);
   }
   void log( const __FlashStringHelper* level_etc, const __FlashStringHelper* msg )
   {
     Serial.print( src );
     Serial.print( level_etc );
     Serial.println( msg );
+
+    syslog(1, src, "FlashStringHelper");
+  }
+
+  static WiFiUDP syslog_wifi;
+
+  static void syslog(int level, const char *source, const char *message)
+  {
+    //if (WiFi.status() != WL_CONNECTED)
+    //  return;
+
+    // Syslog messages should apparently carry a protocol version, various
+    // identifiers, a timestamp and so on.  But it works just fine with a
+    // severity and a message, and really that's all we want or need.
+
+    char header[] = { '<', (char)(level + '0'), '>', '\0' };
+        // the syslog protocol allows 0..191, but we know we'll only need 0..9
+        // (which implicitly means they'll all be 'kernel' messages).
+
+    syslog_wifi.beginPacket(IPAddress(10,23,1,111), 514);
+    syslog_wifi.write(header);
+    syslog_wifi.write(WIFI_HOSTNAME);
+    syslog_wifi.write(' ');
+    syslog_wifi.write(source);
+    syslog_wifi.write(": ");
+    syslog_wifi.write(message);
+    syslog_wifi.endPacket();
   }
 };
+
+WiFiUDP Logger::syslog_wifi;
 
 } // node
